68b7a54d5dedaf48a932c3937a1a26e1622a90ef
TheDevDavide <20186900@itla.edu.do>
2022-10-26 00:13:59 +0200

    Initial commit

---
+++ src/main/java/com/cascainv/CaScaInv/CaScaInvApplication.java
@@ -0,0 +1,13 @@
+package com.cascainv.CaScaInv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaScaInvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaScaInvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cascainv/CaScaInv/CaScaInvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cascainv.CaScaInv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaScaInvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
